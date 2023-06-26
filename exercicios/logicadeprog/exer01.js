43c2faa9685ba0d01f81953727e56496ab1e9803
ViniciusfAraujo <128641765+ViniciusfAraujo@users.noreply.github.com>
2023-06-26 09:01:39 -0300

    atualizada

--- exercicios/logicadeprog/exer01.js
+++ exercicios/logicadeprog/exer01.js
@@ -1,10 +1,18 @@
 // Escreva uma função que recebe 2 numeros e retorne o maior deles
+let x = 100
+let y = 12
+
+const res = (x, y) => x > y ? x : y 
+console.log(res(x, y));
+/*
 function numero(x, y){
-    let x = 7
-    let y = 2
-    let num = [x, y]
-    if(x > y){
-        console.log(`Numero maior é: ${num}`);
-    }
-    
+    return x > y ? x : y
+
+    //if(x > y){
+    //    return x
+    //}else{
+    //    return y
+    //}   
 }
+console.log(numero(x, y));
+*/
