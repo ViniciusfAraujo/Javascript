d00d1ba3e847e97c22296c22c8ce4dd32f0de0df
ViniciusfAraujo <128641765+ViniciusfAraujo@users.noreply.github.com>
2023-07-18 07:10:47 -0300

    teste finalizado

---
+++ livro/teste1.js
@@ -0,0 +1,8 @@
+function simpleArraySum (ar){
+    let soma = 0;
+    for(let i = 0; i < ar.length; i += 1){
+        soma = soma + ar[i]
+    }
+    return soma
+}
+console.log(simpleArraySum([1,2,3,4,10,11]))
