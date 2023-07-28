30aa14750911ce0c746228adac2a7f90e7b0d447
ViniciusfAraujo <128641765+ViniciusfAraujo@users.noreply.github.com>
2023-07-28 10:38:22 -0300

    Aula OK

---
+++ Arrays/array3.js
@@ -0,0 +1,34 @@
+// Filter -> Semepre retorna um array, com a mesma quantidade de 
+//elementos ou menos
+
+// Retorne números maiores que 10
+const numeros = [50,10,3,6,2,7,9,11,33,22,67,17]
+const numerosFiltrados = numeros.filter(valor =>  valor > 10) //Function de callback
+console.log(numerosFiltrados);
+
+/*function callbackFilter(valor){
+    return valor > 10   
+}
+*/
+console.log('---------------------------------');
+
+//Retorne as pessoas que tem o nome com 5 letras ou mais
+//Retorne as pessoas com de 19 anos
+//Retorne as pessoas cujo o nome termine com A
+const pessoas = [
+    {nome: 'Vinicius', idade:23},
+    {nome: 'Ana', idade: 36},
+    {nome: 'Milena', idade: 21},
+    {nome: 'Maria', idade: 17},
+    {nome: 'Rogerio', idade: 32},
+    {nome: 'Joe', idade: 19},
+]
+
+const pessoas2 = pessoas.filter(obj => obj.nome.length > 5)
+console.log(pessoas2);
+
+const pessoaIdade = pessoas.filter(obj => obj.idade > 19)
+console.log(pessoaIdade);
+
+const pessoaA = pessoas.filter(obj => obj.nome.toLowerCase().endsWith('a'))
+console.log(pessoaA);
---
+++ Arrays/array4.js
@@ -0,0 +1,34 @@
+
+const numeros = [50,10,3,6,2,7,9,11,33,22,67,17]
+const dobro = numeros.map(valor => valor * 2)
+console.log(dobro);
+
+console.log('---------------------------------');
+
+/*
+Para cada elemento:
+Retorne apenas uma string com o nome da pessoa
+Remova apenas a chaves 'nome do objeto'
+Adicione uma chave id em cada objeto
+*/ 
+
+const pessoas = [
+    {nome: 'Vinicius', idade:23},
+    {nome: 'Ana', idade: 36},
+    {nome: 'Milena', idade: 21},
+    {nome: 'Maria', idade: 17},
+    {nome: 'Rogerio', idade: 32},
+    {nome: 'Joe', idade: 19},
+]
+
+const nomes = pessoas.map(obj => obj.nome)
+console.log(nomes);
+
+const idades = pessoas.map(obj => ({idade: obj.idade}))
+console.log(idades);
+
+const ids = pessoas.map(function(obj, indice){
+    obj.id = indice
+    return obj
+})
+console.log(ids);
