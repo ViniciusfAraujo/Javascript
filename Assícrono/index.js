663dba69b6ab1a3a787ecb76c28355a44395a463
ViniciusfAraujo <128641765+ViniciusfAraujo@users.noreply.github.com>
2023-09-14 14:19:07 -0300

    ok

---
+++ Assícrono/index.js
@@ -0,0 +1,32 @@
+// Promeses
+function rand(min, max){
+    min *= 1000
+    max *= 1000
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+function esperaAi(msg, tempo){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(msg)
+        }, tempo)
+    })
+}
+
+esperaAi('Conexão com DATABASE', rand(1, 3))
+    .then(resposta => {
+        console.log(resposta);
+        return esperaAi('Buscando os dados da BASE', rand(1, 3))
+    })
+    .then(resposta => {
+        console.log(resposta);
+        return esperaAi('Tratando os dados da BASE', rand(1, 3))
+    })
+    .then(resposta => {
+        console.log(resposta);
+    })
+    .then(resposta => {
+        console.log('Exibindo dados na tela.');
+    })
+    .catch()
+
